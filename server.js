e76277518444e3229e005c803fc59531fda83113
optimm <ayushsaxena823@gmail.com>
2021-11-24 23:02:29 +0530

    deploy

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const server = require('http').Server(app);
+const PORT = process.env.PORT || 3000;
+const io = require('socket.io')(server)
+const { v4: uuidV4 } = require('uuid')
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+  res.redirect(`/${uuidV4()}`)
+})
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room })
+})
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId, userName) => {
+    socket.join(roomId);
+    console.log(roomId,userId);
+    socket.to(roomId).broadcast.emit('user-connected', userId)
+
+    socket.on('message', (message) => {
+      //send message to the same room
+      io.to(roomId).emit('createMessage', message, userName);
+  })
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId)
+    })
+  })
+})
+
+
+
+server.listen(PORT,()=>{
+  console.log("Server Started at port :" + PORT);
+});
